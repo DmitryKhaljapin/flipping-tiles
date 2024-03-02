c1c488cf18fa58cf7dea2d186cb63efee911923b
DmitriX <dodge.241294@gmail.com>
2024-03-02 13:41:39 +0300

    add: clearing selected tiles list action was added

--- src/store/Reducers/selectedTilesReducer.js
+++ src/store/Reducers/selectedTilesReducer.js
@@ -3,12 +3,18 @@ const initialState = {
 }
 
 const ADD_TILE = 'ADD_TILE';
+const CLEAR_SELECTED_TILES_LIST = 'CLEAR_SELECTED_TILES_LIST';
 
 export const selectedTilesReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_TILE': {
             return {
-                selectedTiles: [...state.selectedTiles, action.payload]
+                selectedTiles: [...state.selectedTiles, action.payload],
+            }
+        }
+        case 'CLEAR_SELECTED_TILES_LIST': {
+            return {
+                selectedTiles: [],
             }
         }
         default: {
@@ -18,3 +24,4 @@ export const selectedTilesReducer = (state = initialState, action) => {
 }
 
 export const addTileAction = (payload) => ({type: ADD_TILE, payload});
+export const clearSelectedTilesListAction = () => ({type: CLEAR_SELECTED_TILES_LIST})
