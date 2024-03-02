e040dc6444803748bf3cb1476ca8211a8e3fbf99
DmitriX <dodge.241294@gmail.com>
2024-03-02 13:54:04 +0300

    add: setting tiles disappered action was added

--- src/store/Reducers/tilesReducer.js
+++ src/store/Reducers/tilesReducer.js
@@ -5,10 +5,23 @@ const initialState = {
     tiles: shuffle(createTiles())
 }
 
+const SET_DISAPPERED = 'SET_DISAPPERED';
+
 export const tilesReducer = (state = initialState, action) => {
     switch(action.type) {
+        case 'SET_DISAPPERED': {
+            const newTilesList = state.tiles.map(tile => {
+                if (action.payload.some(selectedTile => selectedTile.id === tile.id)) tile.disappered = true;
+                return tile
+            })
+            return {
+                tiles: newTilesList
+            };
+        }
         default: {
             return state;
         } 
     }
 }
+
+export const setDisapperedAction = (payload) => ({type: SET_DISAPPERED, payload});
