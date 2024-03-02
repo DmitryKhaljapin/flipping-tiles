457eaf4a9d575ac360cfc2bdc182c0fbb481242f
DmitriX <dodge.241294@gmail.com>
2024-03-02 12:59:49 +0300

    add: adding selected tiles action was added

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Card } from '../UI';
 import styles from './Tile.module.scss';
 import cn from 'classnames';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTileAction } from '@/store/Reducers/selectedTilesReducer'; 
 
 export const Tile = ({id, faceColor}) => {
 
-    const [flipped, setFlipped] = useState(false);
+    const dispatch = useDispatch();
+    const selectedTiles = useSelector(store => store.selectedTiles.selectedTiles);
+
+    function clickHandler() {
+        dispatch(addTileAction({id, color: faceColor}));
+    }
 
     return (
         <div className={styles.tile}>
             <div className={cn(styles.inner, {
-                [styles.flipped]: flipped
+                [styles.flipped]: selectedTiles.some(tile => tile.id === id)
             })}
-                onClick={() => setFlipped(true)}
+                onClick={clickHandler}
             >
                 <Card className={styles.face}></Card>
                 <Card className={styles.back}></Card>
--- src/store/Reducers/selectedTilesReducer.js
+++ src/store/Reducers/selectedTilesReducer.js
@@ -2,10 +2,19 @@ const initialState = {
     selectedTiles: [],
 }
 
+const ADD_TILE = 'ADD_TILE';
+
 export const selectedTilesReducer = (state = initialState, action) => {
     switch (action.type) {
+        case 'ADD_TILE': {
+            return {
+                selectedTiles: [...state.selectedTiles, action.payload]
+            }
+        }
         default: {
             return state;
         }
     }
 }
+
+export const addTileAction = (payload) => ({type: ADD_TILE, payload});
